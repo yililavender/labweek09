8ca3e507612180a043bfda3df36824ad92a06f91
yililavender <99084714+yililavender@users.noreply.github.com>
2022-11-01 20:35:14 -0400

    Nov12022

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - 1</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <p>100675355</p>
+        <p>Hui Qiu</p>
+        <p>George Brown College, Toronto</p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
